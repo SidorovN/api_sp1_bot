be8f0990478be763a264ea5d253950d6f8fe1f9f
Nikolay Sidorov <sidorov.n@idaproject.com>
2021-07-21 17:59:22 +0300

    add bot

--- homework.py
+++ homework.py
@@ -1,9 +1,15 @@
+import logging
 import os
 import time
 import requests
 import telegram
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s, %(levelname)s, %(name)s, %(message)s',
+    filename='./main.log', filemode='a'
+)
 
 load_dotenv()
 
@@ -14,12 +20,13 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 # проинициализируйте бота здесь,
 # чтобы он был доступен в каждом нижеобъявленном методе,
 # и не нужно было прокидывать его в каждый вызов
-bot = ...
+bot = telegram.Bot(TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+
+    if homework['status'] != 'approved':
         verdict = 'К сожалению, в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, работа зачтена!'
@@ -27,12 +34,15 @@ def parse_homework_status(homework):
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    homework_statuses = requests.get(url, headers=headers, params=payload)
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(CHAT_ID, message)
 
 
 def main():
@@ -40,11 +50,15 @@ def main():
 
     while True:
         try:
-            ...
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
+            logging.info('Проверил статус')
+            homework = get_homeworks(current_timestamp)
+            if homework['status']:
+                message = parse_homework_status(homework)
+                send_message(message)
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            logging.error(f'Бот упал с ошибкой: {e}')
             time.sleep(5)
 
 
